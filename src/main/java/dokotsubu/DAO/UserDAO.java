ff7bd28bbd8ebb5d2eee4dff818fd7a9365ffbb7
MK-sigh <glycocalyxes@gmail.com>
2026-01-11 19:33:02 +0900

    UserDAOにユーザー存在チェック、登録処理を追加。

--- src/main/java/dokotsubu/DAO/UserDAO.java
+++ src/main/java/dokotsubu/DAO/UserDAO.java
@@ -9,13 +9,33 @@ import dokotsubu.model.User;
 import dokotsubu.util.DBManager;
 
 public class UserDAO {
-    public User findByLogin(User user){
+    //ユーザー登録
+    public boolean resisterUser(User user){
         try(Connection conn = DBManager.getConnection()){
             String sql =
-            "SELECT USER_ID, PASS, NAME,  FROM ACCOUNTS WHERE USER_ID=? AND PASS=?";
+            "INSERT INTO ACCOUNTS (NAME, PASS) VALUES (?,?);";
             PreparedStatement pStmt = conn.prepareStatement(sql);
-            pStmt.setInt(1, user.getUserId());
+            pStmt.setString(1, user.getName());
             pStmt.setString(2, user.getPass());
+
+            int result = pStmt.executeUpdate();
+            if (result !=1){
+                return false;
+            }
+        }catch (SQLException e){
+            e.printStackTrace();
+            return false;
+        }
+        return true;
+    }
+
+    //ユーザー情報の存在チェック
+    public User findUser(User user){
+            try(Connection conn = DBManager.getConnection()){
+                String sql = "SELECT* FROM ACCOUNTS WHERE NAME=? AND PASS=?";
+                PreparedStatement pStmt = conn.prepareStatement(sql);
+                pStmt.setString(1, user.getName());
+                pStmt.setString(2, user.getPass());
 
             ResultSet rs = pStmt.executeQuery();
 
@@ -24,10 +44,12 @@ public class UserDAO {
                 String pass = rs.getString("PASS");
                 String name = rs.getString("NAME");
                 user = new User(userId, pass, name);
+                return user;
             }
+            return null;
         }catch(SQLException e){
             e.printStackTrace();
+            return null;
         }
-        return user;
     }
 }
